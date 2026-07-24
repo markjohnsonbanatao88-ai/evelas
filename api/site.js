739dd2ad03bca63075b49b8fb6ce45a8b9efd21d
mbanatao <markjohnsonbanatao888@gmail.com>
2026-07-24 08:31:43 +0000

    texf

--- api/site.js
+++ api/site.js
@@ -1,21 +1,7 @@
-const fs = require("fs");
-const path = require("path");
-
-const PUBLIC_EMAIL = "esmaniedwin@gmail.com";
-const LEGACY_EMAIL = "evelasenterprise@gmail.com";
-
 module.exports = function handler(req, res) {
-  try {
-    const sourcePath = path.join(process.cwd(), "site.html");
-    const html = fs
-      .readFileSync(sourcePath, "utf8")
-      .replaceAll(LEGACY_EMAIL, PUBLIC_EMAIL);
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.setHeader("Cache-Control", "no-store, max-age=0");
+  res.setHeader("Retry-After", "86400");
 
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.setHeader("Cache-Control", "public, max-age=0, s-maxage=3600");
-    return res.status(200).send(html);
-  } catch (error) {
-    console.error("Website rendering failed", error);
-    return res.status(500).send("Website temporarily unavailable.");
-  }
+  return res.status(503).send("Website is currently offline.");
 };
